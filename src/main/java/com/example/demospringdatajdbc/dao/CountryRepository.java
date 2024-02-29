1f9b62edcc1afd35e80714c568db18d87d36aaa9
Guillaume HUSTA <guillaume.husta@gmail.com>
2024-02-29 17:20:09 +0100

    Update Spring Boot to 2.7.18

--- src/main/java/com/example/demospringdatajdbc/dao/CountryRepository.java
+++ src/main/java/com/example/demospringdatajdbc/dao/CountryRepository.java
@@ -2,15 +2,12 @@ package com.example.demospringdatajdbc.dao;
 
 import com.example.demospringdatajdbc.domain.CountryDTO;
 import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-/**
- * {@link ListCrudRepository} extends {@link org.springframework.data.repository.CrudRepository}.
- */
-public interface CountryRepository extends ListCrudRepository<CountryDTO, String> {
+public interface CountryRepository extends CrudRepository<CountryDTO, String> {
 
     Optional<CountryDTO> findByCode(String code);
 
